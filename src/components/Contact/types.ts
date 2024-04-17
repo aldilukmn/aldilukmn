a630c4c510e81b640a941f010a5df362480a7cf3
aldilukmn <aldilukman99@gmail.com>
2024-04-17 21:55:33 +0700

    feat(modal): add modal pop up after user is submit form

--- src/components/Contact/types.ts
+++ src/components/Contact/types.ts
@@ -13,12 +13,22 @@ export interface ResponseType {
   success: boolean
 }
 
-export interface StateFormType {
+export interface UserType {
   name: string
   email: string
   phone: string
   message: string
+}
+
+export interface StateFormType {
+  user: UserType
   handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
   handleTextareaChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void
   resetState: () => void
+}
+
+export interface ModalFormHookType {
+  isOpen: boolean
+  openModal: () => void
+  closeModal: () => void
 }
